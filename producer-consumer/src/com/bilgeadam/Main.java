1f335e444efd3793528f5e941024c5a3afc347cd
kmsync <tommyjwi@gmail.com>
2022-03-13 13:47:48 +0300

    .

---
+++ producer-consumer/src/com/bilgeadam/Main.java
@@ -0,0 +1,8 @@
+package com.bilgeadam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ producer-consumer/src/com/bilgeadam/ProducerConsumer.java
@@ -0,0 +1,18 @@
+package com.bilgeadam;
+
+public class ProducerConsumer<E> {
+
+    private E queue;
+
+    public void produce(){
+
+
+    }
+
+
+    public void consume() {
+
+
+    }
+
+}
